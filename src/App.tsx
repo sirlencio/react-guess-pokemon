1808d0627efa564875d5599a355a27aa8d0e4c9b
sirlencio <carlos.l.p.251001@gmail.com>
2025-08-26 15:39:28 +0200

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PokemonFeedback from "./components/pokemon-feedback";
 import PokemonForm from "./components/pokemon-form";
 import { GameState, useGameManager } from "./hooks/use-game-manager";
 import ReactConfetti from "react-confetti";
+import { useEffect } from "react";
 
 const App = () => {
   const {
@@ -15,7 +16,11 @@ const App = () => {
     gameState,
   } = useGameManager();
 
-  const {width, height} = useWindowSize();
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    document.title = "Pokémon Guessing Game";
+  }, []);
 
   if (error) {
     return <div className="alert alert-danger text-center">{error}</div>;
@@ -23,21 +28,30 @@ const App = () => {
 
   return (
     <div className="container mx-auto my-5">
-      {
-        gameState === GameState.Correct && (
-          <ReactConfetti 
-            width={width}
-            height={height}
-            numberOfPieces={300}
-            recycle={false}
-          />
-        )
-      }
+      {gameState === GameState.Correct && (
+        <ReactConfetti
+          width={width}
+          height={height}
+          numberOfPieces={300}
+          recycle={false}
+        />
+      )}
       <div className="row justify-content-center">
         <div className="col-12 col-md-8 col-lg-6">
-          <PokemonDisplay pokemon={pokemon} isLoading={isLoading} gameState={gameState}/>
-          <PokemonForm handlePokemonNameSubmit={handlePokemonNameSubmit} gameState={gameState} pokemon={pokemon}/>
-          <PokemonFeedback loadNewPokemon={loadNewPokemon} gameState={gameState}/>
+          <PokemonDisplay
+            pokemon={pokemon}
+            isLoading={isLoading}
+            gameState={gameState}
+          />
+          <PokemonForm
+            handlePokemonNameSubmit={handlePokemonNameSubmit}
+            gameState={gameState}
+            pokemon={pokemon}
+          />
+          <PokemonFeedback
+            loadNewPokemon={loadNewPokemon}
+            gameState={gameState}
+          />
         </div>
       </div>
     </div>
